8890e89d76ec0124433d91a993908c69e2a1243d
jan-lab <tnlatte@outlook.com>
2021-06-29 13:47:49 -0700

    animated sentence

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+
+for (const char of sentence) {
+
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay); // <= 1s delay to make it noticeable. Can dial it down later.
+  
+  delay += 50;
+  
+}
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, delay);
